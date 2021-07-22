fbdcc6b955260f14444e3d1ea86295530f469f80
christopherbanas <cjb5537@rit.edu>
2021-07-21 21:28:02 -0400

    Team box score implemented

--- test.py
+++ test.py
@@ -4,22 +4,52 @@ import time
 
 def main():
     yesterday = str((datetime.now() - timedelta(1)).strftime('%m/%d/%Y'))
-    date = "12/25/2020"
-    games = leaguegamefinder.LeagueGameFinder(
+    date = "7/20/2021"
+    rawGames = leaguegamefinder.LeagueGameFinder(
         date_from_nullable=date,
         date_to_nullable=date,
-        season_type_nullable="Regular Season"
+        season_type_nullable="Playoffs"
     ).get_dict()
 
     GAME_ID_INDEX = 4
+    HOME_AWAY_INDEX = 6
     gameIds = set()
-    for game in games['resultSets'][0]['rowSet']:
+    games = {}
+    for game in rawGames['resultSets'][0]['rowSet']:
         gameId = game[GAME_ID_INDEX]
         if gameId not in gameIds:
             gameIds.add(gameId)
-    boxScores = []
+            if 'vs' in game[HOME_AWAY_INDEX]:  # home team
+                games[gameId] = {"HOME": game}
+            else:
+                games[gameId] = {"AWAY": game}
+        else:  # already in
+            if 'vs' in game[HOME_AWAY_INDEX]:  # home team
+                games[gameId].update({"HOME": game})
+            else:
+                games[gameId].update({"AWAY": game})
+    boxScores = {}
+    for gameId in games:
+        getTeamBoxScore(boxScores, gameId, games[gameId])
     for gameId in gameIds:
         getPlayerBoxScore(boxScores, gameId)
+    print("e")
+
+def getTeamBoxScore(boxScores, gameId, gameDict):
+    headers = ['SEASON_ID', 'TEAM_ID', 'TEAM_ABBREVIATION', 'TEAM_NAME', 'GAME_ID', 'GAME_DATE', 'MATCHUP', 'WL', 'MIN', 'PTS', 'FGM', 'FGA', 'FG_PCT', 'FG3M', 'FG3A', 'FG3_PCT', 'FTM', 'FTA', 'FT_PCT', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PLUS_MINUS']
+    homeBox = homeAwayTeamBoxScore(gameDict['HOME'], headers)
+    awayBox = homeAwayTeamBoxScore(gameDict['AWAY'], headers)
+    boxScores[gameId] = {"TEAM_BOX_SCORE": {"AWAY": awayBox, "HOME": homeBox}}
+
+def homeAwayTeamBoxScore(teamBox, headers):
+    returnBox = []
+    tempDict = {}
+    index = 0
+    for header in headers:
+        tempDict[header] = teamBox[index]
+        index += 1
+    returnBox.append(tempDict)
+    return returnBox
 
 def getPlayerBoxScore(boxScores, gameId):
     fullBox = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=gameId).get_dict()
@@ -41,7 +71,8 @@ def getPlayerBoxScore(boxScores, gameId):
             awayBox.append(box)
         else:  # home team
             homeBox.append(box)
-    boxScores.append({gameId : {"PLAYER_BOX_SCORE": {"AWAY": awayBox, "HOME": homeBox}}})
+    boxScores[gameId].update({"PLAYER_BOX_SCORE": {"AWAY": awayBox, "HOME": homeBox}})
+
 
 
 if __name__ == "__main__":
