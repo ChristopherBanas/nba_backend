a0e84f0e1e3bbf0364f50b06288c15d6e6fb82f1
christopherbanas <cjb5537@rit.edu>
2021-07-25 12:56:34 -0400

    Test heroku

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from server.routes import GetGames, GetStandings
+from app.routes import GetGames, GetStandings
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,5 +9,3 @@ api = Api(app)
 api.add_resource(GetGames, '/games/month/<int:month>/day/<int:day>/year/<int:year>')
 api.add_resource(GetStandings, '/standings')
 
-if __name__ == '__main__':
-    app.run(debug=True)
