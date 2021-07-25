c50fe01591d6be22030f97f20192243f26e9c621
christopherbanas <cjb5537@rit.edu>
2021-07-25 13:08:54 -0400

    Index

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-from app.routes import GetGames, GetStandings
+from app.routes import GetGames, GetStandings, Index
 
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(Index, '/')
 api.add_resource(GetGames, '/games/month/<int:month>/day/<int:day>/year/<int:year>')
 api.add_resource(GetStandings, '/standings')
 
